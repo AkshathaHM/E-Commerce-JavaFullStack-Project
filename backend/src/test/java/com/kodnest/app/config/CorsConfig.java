2b37eff3a070d1d942e040577ef03b2315c57241
AkshathaHM <akshathahmohan@gmail.com>
2026-07-13 11:06:35 +0530

    Cors fix

--- backend/src/test/java/com/kodnest/app/config/CorsConfig.java
+++ backend/src/test/java/com/kodnest/app/config/CorsConfig.java
@@ -6,13 +6,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")           // Try this version too
-                .allowedMethods("*")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(false);
+                .allowCredentials(false)
+                .exposedHeaders("Authorization");
     }
 }
