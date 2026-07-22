019c103a4f6f15238d7d6498ace2928c430cbe88
AkshathaHM <akshathahmohan@gmail.com>
2026-07-22 12:47:07 +0530

    updated functio

--- backend/src/main/java/com/kodnest/app/adminControllers/AdminProductController.java
+++ backend/src/main/java/com/kodnest/app/adminControllers/AdminProductController.java
@@ -50,7 +50,7 @@ public class AdminProductController {
     }
 
     @DeleteMapping("/delete")
-    @CrossOrigin(allowedOriginPatterns = "*", allowedHeaders = "*", allowCredentials = "true")
+    @CrossOrigin(origins = {"http://localhost:5174", "http://localhost:5173", "http://127.0.0.1:5174", "http://127.0.0.1:5173", "https://e-commerce-java-full-stack-project-five.vercel.app", "https://e-commerce-java-full-stack-project-seven.vercel.app", "https://e-commerce-javafullstack-project-2.onrender.com"}, allowCredentials = "true")
     public ResponseEntity<?> deleteProduct(@RequestBody Map<String, Integer> requestBody) {
         try {
             Integer productId = requestBody.get("productId");
