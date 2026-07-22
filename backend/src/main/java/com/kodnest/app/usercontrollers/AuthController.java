3001d904bd89641cbce72b92de61670b1ad36d2e
AkshathaHM <akshathahmohan@gmail.com>
2026-07-22 11:36:41 +0530

    updated function

--- backend/src/main/java/com/kodnest/app/usercontrollers/AuthController.java
+++ backend/src/main/java/com/kodnest/app/usercontrollers/AuthController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ import com.kodnest.app.userservices.AuthServiceContract;
 
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @CrossOrigin(
@@ -89,7 +91,6 @@ public class AuthController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
         }
     }
-}
 
     @GetMapping("/me")
     public ResponseEntity<?> me(HttpServletRequest request) {
@@ -105,3 +106,5 @@ public class AuthController {
         body.put("address", user.getAddress());
         return ResponseEntity.ok(body);
     }
+
+}
